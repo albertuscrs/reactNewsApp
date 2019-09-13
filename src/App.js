b80102a1d35f16f335cc93d3f31bd402951f962d
Albertus Christyadhi <achristyadhi@gmail.com>
2019-09-13 16:40:18 +0700

    Update: styling and add news api from algolia

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import list from './list';
-import { Container, Row, Form, FormGroup } from 'react-bootstrap';
-import './App.css';
-import { id } from 'postcss-selector-parser';
+// import list from './list';
+import { Container, Row, FormGroup } from 'react-bootstrap';
+
+const DEFAULT_QUERY = 'react';
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = 100;
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage='
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}&${PARAM_HPP}${DEFAULT_HPP}`;
+console.log(url);
 
 function isSearched(searchTerm){
   return function(item){
@@ -16,17 +27,44 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     }
 
     this.removeItem = this.removeItem.bind(this);
     this.searchValue = this.searchValue.bind(this);
+    this.fetchTopStories = this.fetchTopStories.bind(this);
+    this.setTopStories = this.setTopStories.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  setTopStories(result){
+    const { hits, page } = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits]
+    this.setState({ result: { hits: updatedHits, page } });
+  }
+
+  fetchTopStories(searchTerm, page){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(response => response.json())
+      .then(result => this.setTopStories(result))
+      .catch(e => e);
+  }
+
+  componentDidMount(){
+    this.fetchTopStories(this.state.searchTerm, DEFAULT_PAGE);
+  }
+
+  onSubmit(event){
+    this.fetchTopStories(this.state.searchTerm, DEFAULT_PAGE);
+    event.preventDefault();
   }
 
   removeItem(id){
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
+    const { result } = this.state;
+    const updatedList = result.hits.filter(item => item.objectID !== id);
+    this.setState({result: {...result, hits: updatedList}});
   }
 
   searchValue(event){
@@ -34,36 +72,48 @@ class App extends Component {
   }
 
   render(){
-    const { list, searchTerm } = this.state;
+    const { result, searchTerm } = this.state;
+
+    const page = (result && result.page) || 0;
+
     console.log(this);
     return (
-        <div className="App">
-
+        <div className='col-sm-10 offset-1'>
           <Container>
             <Row>
-              <div className="jumbotron">
+              <div className="jumbotron text-center">
                 <Search
                   onChange={ this.searchValue }
                   value={ searchTerm }
+                  onSubmit= { this.onSubmit }
                 >News App</Search>
               </div>
             </Row>
           </Container>
-
-          <Table
-            list={ list }
+          { result &&
+            <Table
+            list={ result.hits }
             searchTerm={ searchTerm }
             removeItem={ this.removeItem }
-          />
-          
+            />
+          }
+          <div className="text-center alert">
+            <Button
+              className="btn btn-success btn-sm"
+              onClick={ () => this.fetchTopStories(searchTerm, page + 1) }
+            >
+              Load More
+            </Button>
+          </div>
       </div>
     );
   }
 }
 
-const Search = ({ onChange, value, children }) => {
+const Search = ({ onChange, value, children, onSubmit }) => {
   return(
-    <Form>
+    <form onSubmit={ onSubmit }>
+      <FormGroup>
        <h1 style={{ fontWeight: 'bold' }}>{ children }</h1> <hr style={{ border: '2px solid black', width: '100px' }} />
         <div className="input-group">
           <input 
@@ -71,7 +121,7 @@ const Search = ({ onChange, value, children }) => {
             type="text"
             onChange={ onChange }
             value={ value }
-          />
+            />
           <span className="input-group-btn">
             <button
               className="btn btn-primary searchBtn"
@@ -80,8 +130,9 @@ const Search = ({ onChange, value, children }) => {
               Search
             </button>
           </span>
-          </div>
-    </Form>
+        </div>
+      </FormGroup>
+    </form>
   )
 }
 
@@ -89,16 +140,19 @@ const Table = ({ list, searchTerm, removeItem }) => {
   return(
     <div>
       {
-        list.filter( isSearched(searchTerm) ).map(item =>
+        // list.filter( isSearched(searchTerm) ).map(item =>
+        list.map(item =>
           <div key={ item.objectID }>
-              <h1>  <a href = { item.url }>{ item.title }</a> by { item.author } </h1>
-              <h4> { item.num_comments } Comments | { item.points } Points </h4>
-              <Button
-                type="button"
-                onClick={ () => removeItem(item.objectID) }
-              >
-                Remove me
-              </Button>
+              <h1>  <a href = { item.url }>{ item.title }</a></h1>
+              <h4> { item.author } | { item.num_comments } Comments | { item.points } Points 
+                <Button
+                  className="btn btn-danger btn-sm"
+                  type="button"
+                  onClick={ () => removeItem(item.objectID) }
+                >
+                  Remove me
+                </Button>
+              </h4> <hr />
           </div>
         )
       }
@@ -106,10 +160,11 @@ const Table = ({ list, searchTerm, removeItem }) => {
   )
 }
 
-const Button = ({ onClick, children }) =>
+const Button = ({ onClick, children, className='' }) =>
     <button
-        onClick={ onClick }>
-        { children }
+      className={ className }
+      onClick={ onClick }>
+      { children }
     </button>
 
 export default App;
