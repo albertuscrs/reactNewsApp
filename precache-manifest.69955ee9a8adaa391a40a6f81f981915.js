07514a1c99b015ee713644b9af7e46c4b176d3a8
Albertus Christyadhi <achristyadhi@gmail.com>
2019-09-15 20:43:12 +0700

    Updates

---
+++ precache-manifest.69955ee9a8adaa391a40a6f81f981915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a79cd95c32b9df56b2ec6bbe17ffc3",
+    "url": "/react-news-app/index.html"
+  },
+  {
+    "revision": "b9dcd3b25520cc1846fa",
+    "url": "/react-news-app/static/css/main.4f399203.chunk.css"
+  },
+  {
+    "revision": "68bf1ade9c4e563cac3e",
+    "url": "/react-news-app/static/js/2.104fc1d4.chunk.js"
+  },
+  {
+    "revision": "b9dcd3b25520cc1846fa",
+    "url": "/react-news-app/static/js/main.bb1580f3.chunk.js"
+  },
+  {
+    "revision": "ef780e0212a709c4d21d",
+    "url": "/react-news-app/static/js/runtime~main.f9751060.js"
+  }
+]);
